68fc97467c4dc78dd59e273420c46116271679f7
WS JI <ws0731@naver.com>
2019-08-02 23:25:54 +0900

    Create 2577.py

---
+++ 1000-9999/2577.py
@@ -0,0 +1,16 @@
+'''
+2577번
+숫자의 개수
+'''
+
+p0,p1,p2,p3,p4,p5,p6,p7,p8,p9=0,0,0,0,0,0,0,0,0,0
+
+a=int(input())
+b=int(input())
+c=int(input())
+result=str(a*b*c)
+for i in range(len(result)):
+    exec("p"+str(result[i])+"+=1")
+    
+for i2 in range(10):
+    print(eval("p"+str(i2)))
