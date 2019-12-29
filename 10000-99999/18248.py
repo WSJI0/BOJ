5b0a27754f9f03fb970e980ea8956128956434c1
WSJI0 <lifedev@naver.com>
2019-12-30 00:36:12 +0900

    Create 18248.py

---
+++ 10000-99999/18248.py
@@ -0,0 +1,14 @@
+'''
+18248번
+제야의 종
+'''
+
+n,m=list(map(int,input().split()))
+
+
+iA=[]
+jA=[]
+for _ in range(n):
+    i,j=list(map(int,input().split()))
+        iA.append(i)
+        jA.append(j)
