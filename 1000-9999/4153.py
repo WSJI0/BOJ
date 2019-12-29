73ef13306122f15010876150b585f799562135d4
WSJI0 <lifedev@naver.com>
2019-12-29 11:02:33 +0900

    Update 4153.py

---
+++ 1000-9999/4153.py
@@ -0,0 +1,21 @@
+'''
+4153번
+직각삼각형
+'''
+
+
+coor=[1,1,1]
+while(coor!=[0,0,0]):
+    ans=''
+    i,j,k=list(map(int,input().split()))
+    coor[0]=i
+    coor[1]=j
+    coor[2]=k
+
+    if max(coor)**2-coor[coor.index(max(coor))-1]**2-coor[coor.index(max(coor))-2]**2==0:
+        ans='right'
+    else:
+        ans='wrong'
+
+    if i!=0 and j!=0 and k!=0:
+        print(ans)
