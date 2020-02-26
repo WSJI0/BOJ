9a01692d51bbe468def9d422f623dc71e7c55ae2
WSJI0 <lifedev@naver.com>
2020-02-26 14:37:49 +0900

    Update 2231.py

--- 1000-9999/2231.py
+++ 1000-9999/2231.py
@@ -3,4 +3,26 @@
 분해합
 '''
 
-ㅁ
+n=int(input())
+
+if n<19:
+    if n%2==0:
+        print(n//2)
+    else:
+        print(0)
+else:
+    for i in range(n-(len(str(n))*9),n):
+        iSplit=map(int,list(str(i)))
+
+        sum=0
+
+        for i2 in iSplit:
+            sum+=i2
+        sum+=i
+        
+        if sum==n:
+            print(i)
+            break
+
+        if i==n-1:
+            print(0)
