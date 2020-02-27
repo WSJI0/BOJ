84bfec31e754604e3ce0894e5d32b52062ebec95
WSJI0 <lifedev@naver.com>
2020-02-28 01:48:08 +0900

    Update 7568.py

--- 1000-9999/7568.py
+++ 1000-9999/7568.py
@@ -2,3 +2,19 @@
 7568번
 덩치
 '''
+
+n=int(input())
+
+people=[[],[]]
+xy=[]
+
+for i in range(n):
+    x,y=map(int,input().split())
+    people[0].append(x)
+    people[1].append(y)
+    xy.append([x*y,i])
+
+xy=sorted(xy,reverse=True)
+
+for j in range(n):
+    if people[0][xy[j]]
