db2ae41f792c834fde345aef68a509b951a0182c
WSJI0 <lifedev@naver.com>
2020-03-28 17:11:08 +0900

    Update 7568.py

--- 1000-9999/7568.py
+++ 1000-9999/7568.py
@@ -6,15 +6,29 @@
 n=int(input())
 
 people=[[],[]]
-xy=[]
+result=[]
 
 for i in range(n):
     x,y=map(int,input().split())
     people[0].append(x)
     people[1].append(y)
-    xy.append([x*y,i])
 
-xy=sorted(xy,reverse=True)
 
-for j in range(n):
-    if people[0][xy[j]]
+for j in range(len(people[0])):
+    grade=1
+    for k in range(len(people[0])):
+        if people[0][j]<people[0][k] and people[1][j]<people[1][k]:
+            grade+=1
+    result.append(str(grade))
+
+print(' '.join(result))
+
+
+'''
+5
+55 185
+58 183
+88 186
+60 175
+46 155
+'''
