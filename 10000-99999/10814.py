77dd46fa8470b348c15a5106ed72ff20b82dbb33
WSJI0 <lifedev@naver.com>
2020-04-03 16:30:52 +0900

    Update 10814.py

--- 10000-99999/10814.py
+++ 10000-99999/10814.py
@@ -8,7 +8,7 @@ n=int(input())
 member=[]
 for i in range(n):
     age, name=input().split()
-    member.append([age,i,name])
+    member.append([int(age),i,name])
 
 member.sort()
 for j in range(n):
