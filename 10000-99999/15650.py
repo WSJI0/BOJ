a985d4698403ea704241adecc29846afc40ebd79
WSJI0 <lifedev@naver.com>
2020-04-03 23:37:37 +0900

    Update 15650.py

--- 10000-99999/15650.py
+++ 10000-99999/15650.py
@@ -2,3 +2,26 @@
 15650번
 N과 M (2)
 '''
+
+n,m=map(int,input().split())
+ 
+check=[0]*(n+1)
+t=[0]*m
+ 
+def make(n, m, l):
+    if l==m:
+        for i in range(m):
+            print (t[i], end=' ')
+        print()            
+        return
+
+    for i in range(1, n+1):
+        if check[i]==0:
+            t[l]=i
+            for j in range(i+1):
+                check[j]=1
+            make(n,m,l+1)
+            for k in range(i+1):
+                check[k]=0
+
+make(n,m,0)
