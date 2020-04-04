2611a2881c717c3354e3723e50111df05e858de0
WSJI0 <lifedev@naver.com>
2020-04-04 22:20:18 +0900

    Update 1026.py

--- 1000-9999/1026.py
+++ 1000-9999/1026.py
@@ -2,3 +2,15 @@
 1026번
 보물
 '''
+
+n=int(input())
+
+A=list(map(int,input().split()))
+B=list(map(int,input().split()))
+A.sort()
+B.sort(reverse=True)
+
+sum=0
+for i in range(n):
+    sum+=A[i]*B[i]
+print(sum)
