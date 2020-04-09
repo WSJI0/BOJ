64ab5693e7e56f76fb57872be44cd82c13eff7da
WSJI0 <lifedev@naver.com>
2020-04-10 00:26:57 +0900

    Create 2609.py

---
+++ 1000-9999/2609.py
@@ -0,0 +1,21 @@
+'''
+2609번
+최대공약수와 최소공배수
+'''
+
+n, m=map(int,input().split())
+
+MAX=max(n,m)
+MIN=min(n,m)
+
+for i in range(1, MAX%MIN+1):
+    if n%i==0 and m%i==0:
+        GCD=i
+
+for j in range(MAX,n*m+1,MAX):
+    if j%n==0 and j%m==0:
+        LCM=j
+        break
+
+print(GCD)
+print(LCM)
