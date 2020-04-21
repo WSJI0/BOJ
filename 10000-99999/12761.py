2152e06cc209361668a6367e529e8eddcc15d11f
WSJI0 <lifedev@naver.com>
2020-04-21 14:38:04 +0900

    Update 12761.py

--- 10000-99999/12761.py
+++ 10000-99999/12761.py
@@ -5,19 +5,42 @@
 
 from collections import deque
 import sys
- 
-def BFS(start):
-    graph={}
+
+def BFS(start, end):
+    global a,b
     visited={}
     q=deque()
     q.append([start,0])
 
     while len(q)>0:
         node=q.popleft()
+        if node[0]==end:
+            return node[1]
+        if node[0]+1<=100000 and (node[0]+1) not in visited:
+            visited[node[0]+1]=1
+            q.append([node[0]+1, node[1]+1])
+        if node[0]-1>=0 and (node[0]-1) not in visited:
+            visited[node[0]-1]=1
+            q.append([node[0]-1, node[1]+1])
+        if node[0]+a<=100000 and (node[0]+a) not in visited:
+            visited[node[0]+a]=1
+            q.append([node[0]+a, node[1]+1])
+        if node[0]+b<=100000 and (node[0]+b) not in visited:
+            visited[node[0]+b]=1
+            q.append([node[0]+b, node[1]+1])
+        if node[0]-a>=0 and (node[0]-a) not in visited:
+            visited[node[0]-a]=1
+            q.append([node[0]-a, node[1]+1])
+        if node[0]-b>=0 and (node[0]-b) not in visited:
+            visited[node[0]-b]=1
+            q.append([node[0]-b, node[1]+1])
+        if node[0]*a<=100000 and (node[0]*a) not in visited:
+            visited[node[0]*a]=1
+            q.append([node[0]*a, node[1]+1])
+        if node[0]*b<=100000 and (node[0]*b) not in visited:
+            visited[node[0]*b]=1
+            q.append([node[0]*b, node[1]+1])
 
-        if node not in visited:
-            visited[node]=True
-            q.extend(graph[node])
-    return visited
 
 a, b, n, m=map(int, sys.stdin.readline().split())
+print(BFS(n,m))
