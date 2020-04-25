cf7ed1b00ff144c57589df18f048bf7b80e2f1af
WSJI0 <lifedev@naver.com>
2020-04-26 01:39:47 +0900

    Update 13398.py

--- 10000-99999/13398.py
+++ 10000-99999/13398.py
@@ -6,73 +6,27 @@
 import sys
 
 n=int(sys.stdin.readline())
-a=list(map(int, sys.stdin.readline().split()))
+s=list(map(int, sys.stdin.readline().split()))
 
 if n==1:
-    print(a[0])
+    print(s[0])
 else:
-    stack=[]
-    m=[]
-    M=0
-    for i in a:
-        if i>=0:
-            if len(stack)==0:
-                stack.append(i)
-            else:
-                if stack[-1]>=0:
-                    stack[-1]+=i
-                    M-=1
-                else:
-                    stack.append(i)
+    d=[[s[0],s[0]]]
+    ans=s[0]
+
+    for i in range(1,n):
+        d.append([s[i],s[i]])
+
+        if d[i-1][0]+s[i]>=s[i]:
+            d[i][0]=d[i-1][0]+s[i]
         else:
-            stack.append(i)
-            m.append(M)
-        M+=1
-
-    maxValue=0
-    toDelete=0
-    for j in m:
-        if j!=0 and j!=len(stack)-1:
-            if stack[j-1]+stack[j+1]>maxValue:
-                maxValue=stack[j-1]+stack[j+1]
-                toDelete=j    
-
-    if toDelete!=0:
-        stack.pop(toDelete)
-        toSum=stack.pop(toDelete)
-        stack[toDelete-1]+=toSum
-
-    s=[]
-    for k in stack:
-        if k>=0:
-            s.append(k)
+            d[i][0]=s[i]
+        
+        if d[i-1][0]>=d[i-1][1]+s[i]:
+            d[i][1]=d[i-1][0]
         else:
-            if len(s)==0:
-                s.append(k)
-            else:
-                if s[-1]<0:
-                    s[-1]+=k
-                else:
-                    s.append(k)
-
-    print(s)
-    last=len(s)-1
-    ans=0
-    if len(s)%2==0:
-        if s[0]>0:
-            for t in range(last, -1, -2):
-                ans=max(ans, sum(s[:t]))
-        else:
-            for t in range(last, 0, -2):
-                print(s[:t])
-                ans=max(ans, sum(s[1:t]))
-
-    else:
-        if s[0]>0:
-            for t in range(last, -1, -2):
-                ans=max(ans, sum(s[:t]))
-        else:
-            for t in range(last, 0, -2):
-                ans=max(ans, sum(s[1:t]))
+            d[i][1]=d[i-1][1]+s[i]
+        if d[i][0]>=ans or d[i][1]>=ans:
+            ans=max(d[i][0],d[i][1])
 
     print(ans)
