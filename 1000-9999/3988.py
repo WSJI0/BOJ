25923e5ea7403daef46594d0532cb0d13a80ed6f
WSJI0 <lifedev@naver.com>
2020-04-27 13:22:14 +0900

    Update 3988.py

--- 1000-9999/3988.py
+++ 1000-9999/3988.py
@@ -10,19 +10,21 @@ n, k=map(int, sys.stdin.readline().split())
 num=list(map(int, sys.stdin.readline().split()))
 num.sort()
 
-for _ in range(k):
-    left=num[1]-num[0]
-    right=num[-1]-num[-2]
-    if left>right:
-        num.pop()
-    elif left<right:
-        num.pop(0)
-    else:
-        left=num[len(num)//2]-num[0]
-        right=num[-1]-num[len(num)//2]
-        if left>right:
-            num.pop(0)
-        else:
-            num.pop()
+d=[]
+ans=9999999999999
+i=0
+J=1
+for i in range(n-k):
+    if len(d)!=0:
+        if d[0]<=i:
+            d.pop(0)
 
-print(num)
+    for j in range(1, i+n-k):
+        if len(d)!=0:
+            if num[j]-num[j-1]<=num[d[0]]-num[d[0]-1]:
+                d.pop()
+        d.append(j)
+
+    ans=min(ans,num[d[0]]-num[d[0]-1]+num[i+n-k-1]-num[i])
+
+print(ans)
