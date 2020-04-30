b9a7cd1483402f67702f1cbb049919bb1bbb25ba
WSJI0 <lifedev@naver.com>
2020-04-30 17:54:04 +0900

    Create 12015.py

---
+++ 10000-99999/12015.py
@@ -0,0 +1,18 @@
+'''
+12015번
+가장 긴 증가하는 부분 수열 2
+'''
+
+import sys
+
+n=int(sys.stdin.readline())
+a=list(map(int, sys.stdin.readline().split()))
+
+cur=a[0]
+cnt=1
+for i in range(1,n):
+    if a[i]>cur:
+        cur=a[i]
+        cnt+=1
+print(cnt)
+
