fffe37ee350fa2788acc255a60dcb7166d200530
WSJI0 <lifedev@naver.com>
2020-04-30 17:54:02 +0900

    Create 2490.py

---
+++ 1000-9999/2490.py
@@ -0,0 +1,13 @@
+'''
+2490번
+윷놀이
+'''
+
+yMap=['E','A','B','C','D']
+y1=list(map(int,input().split()))
+y2=list(map(int,input().split()))
+y3=list(map(int,input().split()))
+
+print(yMap[y1.count(0)])
+print(yMap[y2.count(0)])
+print(yMap[y3.count(0)])
