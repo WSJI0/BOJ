71e10cdbc33ed65fb674962c2b2f88df713970fc
WSJI0 <lifedev@naver.com>
2020-05-27 00:38:26 +0900

    Create 1034.py

---
+++ 1000-9999/1034.py
@@ -0,0 +1,18 @@
+import sys
+
+n, m=map(int, sys.stdin.readline().split())
+
+board=[]
+for i in range(n):
+    a=sys.stdin.readline().rstrip()
+    board.append([int(i) for i in a])
+
+k=int(sys.stdin.readline())
+
+switch=[0]*len(board[0])
+
+for j in board:
+    for l in range(len(j)):
+        if j[l]==0:
+            switch[l]+=1
+
