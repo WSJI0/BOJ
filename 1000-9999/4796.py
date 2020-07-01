c91cdccbed7d9644b330c6d64c21a014ddbae0ac
WSJI0 <lifedev@naver.com>
2020-07-02 00:16:53 +0900

    Update 4796.py

--- 1000-9999/4796.py
+++ 1000-9999/4796.py
@@ -2,3 +2,17 @@
 4796번
 캠핑
 '''
+
+import sys
+input=sys.stdin.readline
+
+case=1
+while True:
+    ans=0
+    l, p, v=map(int, input().split())
+    if l==0 and p==0 and v==0: break
+
+    ans+=min(v%p, l)
+    ans+=v//p*l
+    print("Case "+str(case)+": "+str(ans))
+    case+=1
