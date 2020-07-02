95fd018b22779d46a97cff72a8edbe5f1e5fa36a
WSJI0 <lifedev@naver.com>
2020-07-03 01:40:46 +0900

    Create 1725.py

---
+++ 1000-9999/1725.py
@@ -0,0 +1,36 @@
+'''
+1725번
+히스토그램
+'''
+
+import sys
+input=sys.stdin.readline
+
+n=int(input())
+
+his=[]
+for _ in range(n):
+    his.append(int(input()))
+
+stack=[]
+ans=0
+for i in range(n):
+    left=i
+    while(len(stack)!=0 and his[stack[-1]]>his[i]):
+        height=his[stack.pop()]
+        width=i
+        if len(stack)!=0:
+            width=i-stack[-1]-1
+        if ans<width*height:
+            ans=width*height
+    stack.append(i)
+
+while(len(stack)!=0):
+    height=his[stack.pop()]
+    width=n
+    if len(stack)!=0:
+        width=n-stack[-1]-1
+    if ans<width*height:
+        ans=width*height
+
+print(ans)
