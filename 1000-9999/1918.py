ea52866311f008c5b4f739f8bc58c8a5d1c6ca0f
WSJI0 <lifedev@naver.com>
2020-07-08 17:49:38 +0900

    Update 1918.py

--- 1000-9999/1918.py
+++ 1000-9999/1918.py
@@ -7,13 +7,16 @@ import sys
 input=sys.stdin.readline
 
 a=list(input().rstrip())
+a.insert(0, '(')
+a.append(')')
 
 def com(n):
     dic={
-        '+':1,
-        '-':1,
-        '*':0,
-        '/':0
+        '*':1,
+        '/':1,
+        '+':0,
+        '-':0,
+        '(':-1
     }
     return dic[n]
 
@@ -21,15 +24,16 @@ stack=[]
 for i in a:
     if i.isalpha():
         print(i, end='')
+    elif i=='(':
+        stack.append(i)
+    elif i==')':
+        while stack[-1]!='(':
+            print(stack.pop(), end='')
+        stack.pop()
     else:
-        if 
-        if len(stack)==0:
-            stack.append(i)
-        else:
-            if com(i)<=com(stack[-1]):
-                print(stack.pop(), end='')
-                stack.append(i)
-            else:
-                stack.append(i)
+        while len(stack)!=0 and stack[-1]!='(' and com(i)<=com(stack[-1]):
+            print(stack.pop(), end='')
+        stack.append(i)
+        
 for i in stack:
     print(i, end='')
