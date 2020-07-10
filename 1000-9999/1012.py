894afd89f2ec0e61d60447f4f55208cefacb4f02
WSJI0 <lifedev@naver.com>
2020-07-10 13:39:51 +0900

    Update 1012.py

--- 1000-9999/1012.py
+++ 1000-9999/1012.py
@@ -3,28 +3,53 @@
 유기농 배추
 '''
 
-testCase=int(input())
+import sys
+input=sys.stdin.readline
 
-maps=[]
+def dfs(graph, node):
+    visited=[]
+    stack=[node]
 
-def check(n,m):
-    if n<0:
-        pass
+    while stack:
+        n=stack.pop()
+        if n not in visited:
+            visited.append(n)
+            stack.extend(graph[n])
+    return visited
 
-for _ in range(testCase):
-    m,n,k=list(map(int,input().split()))
+t=int(input())
 
-    for i in range(m):
-        maps.append([])
-        for _ in range(n):
-            maps[i].append(0)
-    
-    for _ in range(k):
-        x,y=list(map(int,input().split()))
-        maps[y][x]=1
-    
-    for j in range(len(maps)-1):
-        for k in range(len(maps[j])-1):
-            if maps[j-1][k-1]==1 or maps[j+1][k-1]==1 or maps[j-1][k+1]==1 or maps[j+1][k+1]==1:
-                maps[j][k]=0
-    print(maps)
+for _ in range(t):
+    m, n, k=map(int, input().split())
+    graph={}
+    for i in range(k):
+        x, y=map(int, input().split())
+        if (x,y) not in graph:
+            graph[(x,y)]=[]
+        if x>0 and (x-1, y) in graph:
+            graph[(x,y)].append((x-1, y))
+            graph[(x-1,y)].append((x,y))
+        if y>0 and (x, y-1) in graph:
+            graph[(x,y)].append((x,y-1))
+            graph[(x,y-1)].append((x,y))
+        if x<m-1 and (x+1, y) in graph:
+            graph[(x,y)].append((x+1,y))
+            graph[(x+1,y)].append((x,y))
+        if y<n-1 and (x,y+1) in graph:
+            graph[(x,y)].append((x,y+1))
+            graph[(x,y+1)].append((x,y))
+
+    visited=[]        
+    cnt=0
+    for y in range(n):
+        for x in range(m):
+            if (x, y) in graph and (x, y) not in visited:
+                stack=[(x,y)]
+                cnt+=1
+                while stack:
+                    n=stack.pop()
+                    if n not in visited:
+                        visited.append(n)
+                        stack.extend(graph[n])
+
+    print(cnt)
