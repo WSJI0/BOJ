65fddfc4fb81a8caf1d444eb6333d27d2b59fbfc
WSJI0 <lifedev@naver.com>
2020-07-13 01:22:04 +0900

    Update bfs.py

--- Algorithm/bfs.py
+++ Algorithm/bfs.py
@@ -3,20 +3,28 @@ BFS
 너비 우선 탐색
 '''
 
-from collections import deque
+graph={
+    0:{1,2},
+    1:{0,3,5},
+    2:{0,6,8},
+    3:{1,4},
+    4:{3,5},
+    5:{1,4},
+    6:{2,7,8},
+    7:{6},
+    8:{2,6}
+}
 
-graph={}
- 
-def BFS(graph, start):
-    visited={}
-    q=deque()
-    q.append(start)
-
-    while len(q)>0:
-        node=q.popleft()
-        if node not in visited:
-            visited[node]=True
-            q.extend(graph[node])
+def bfs(graph, node):
+    visited=[]
+    queue=[node]
+    
+    while queue:
+        n=queue.pop(0)
+        if n not in visited:
+            visited.append(n)
+            queue.extend(graph[n])
     return visited
+                
 
-print(BFS(graph,'1'))
+print(bfs(graph,0))
