bd740d5426d5e1b382e7255db8f26543412b393c
WSJI0 <lifedev@naver.com>
2020-07-14 00:32:00 +0900

    Update 2206.py

--- 1000-9999/2206.py
+++ 1000-9999/2206.py
@@ -4,11 +4,43 @@
 '''
 
 import sys
+from collections import deque
 input=sys.stdin.readline
 
-n, m=map(int, input().split())
+N, M=map(int, input().split())
 
 board=[]
-for _ in range(board):
-    board.append(list(input().rstrip()))
+for _ in range(N):
+    board.append(list(map(int, input().rstrip())))
 
+q=deque()
+q.append((0,0,1))
+visited={}
+cnt=0
+find=False
+move=[[1,0], [0,1], [-1,0], [0,-1]]
+
+while q:
+    for _ in range(len(q)):
+        n=q.popleft()
+        if n not in visited:
+            visited[n]=True
+            if n[0]==M-1 and n[1]==N-1:
+                find=True
+                break
+
+            for m in move:
+                if 0<=n[0]+m[0]<M and 0<=n[1]+m[1]<N:
+                    if board[n[1]+m[1]][n[0]+m[0]]==0:
+                        q.append((n[0]+m[0], n[1]+m[1], n[2]))
+                    else:
+                        if n[2]==1:
+                            q.append((n[0]+m[0], n[1]+m[1], 0))
+    if find: break
+    cnt+=1
+
+
+if find:
+    print(cnt+1)
+else:
+    print(-1)
