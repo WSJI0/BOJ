43243d4283fb2976ebacfdd49c5ef586adae5e2e
WSJI0 <lifedev@naver.com>
2020-07-13 23:42:20 +0900

    Update 5014.py

--- 1000-9999/5014.py
+++ 1000-9999/5014.py
@@ -4,6 +4,27 @@
 '''
 
 import sys
+from collections import deque
 input=sys.stdin.readline
 
 f, s, g, u, d=map(int, input().split())
+
+visited={}
+q=deque()
+q.append(s)
+
+cnt=0
+while q:
+    for i in range(len(q)):
+        n=q.popleft()
+        if n not in visited:
+            visited[n]=True
+            if n+u<=f: q.append(n+u)
+            if n-d>0: q.append(n-d)
+    if g in visited: break
+    cnt+=1
+
+if g in visited:
+    print(cnt)
+else:
+    print("use the stairs")
