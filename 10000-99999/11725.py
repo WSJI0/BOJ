c1ee8fded40749338f3b4dc22c9b285be7bcc3b3
WSJI0 <lifedev@naver.com>
2020-07-15 13:41:11 +0900

    Update 11725.py

--- 10000-99999/11725.py
+++ 10000-99999/11725.py
@@ -6,48 +6,11 @@
 import sys
 input=sys.stdin.readline
 
-class Node:
-    def __init__(self, data):
-        self.data=data
-        self.left=None
-        self.right=None
-
-    def __str__(self):
-        return str(self.data)
-
-class Tree:
-    def __init__(self):
-        self.root=None
-
-    def preorderTraversal(self, node):
-        print(node, end='')
-        if not node.left==None:
-            self.preorderTraversal(node.left)
-        if not node.right==None:
-            self.preorderTraversal(node.right)
-    
-    def inorderTraversal(self, node):
-        if not node.left==None:
-            self.inorderTraversal(node.left)
-        print(node, end='')
-        if not node.right==None:
-            self.inorderTraversal(node.right)
-
-    def postorderTraversal(self, node):
-        if not node.left==None:
-            self.postorderTraversal(node.left)
-        if not node.right==None:
-            self.postorderTraversal(node.right)
-        print(node, end='')
-
-    def connectLeft(self, node, left_node):
-        if self.root==None:
-            self.root=node
-        node.left=left_node
-
-    def connectRight(self, node, right_node):
-        if self.root==None:
-            self.root=node
-        node.right=right_node
+graph={}
 
 n=int(input())
+ans=[0]*(n+1)
+
+for _ in range(n-1):
+    a, b=map(int, input().split())
+    
