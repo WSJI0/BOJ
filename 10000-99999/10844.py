7007ee42cbdd46968442b1cc7f786961abafeadb
WSJI0 <lifedev@naver.com>
2020-07-15 13:41:09 +0900

    Create 10844.py

---
+++ 10000-99999/10844.py
@@ -0,0 +1,24 @@
+'''
+10844번
+쉬운 계단 수
+'''
+
+import sys
+input=sys.stdin.readline
+
+n=int(input())
+
+dp=[1,2,3,4,5,6,7,8,9]
+cnt=0
+while True:
+    a=[]
+    if n==1: break
+    for i in range(len(dp)):
+        if dp[i]>0:
+            a.append(dp[i]-1)
+        if dp[i]<9:
+            a.append(dp[i]+1)
+    dp=a  
+    cnt+=1
+    if cnt==n-1: break
+print(len(dp)%1000000000)
