c52dc6c70bf6a07c8599652b8cd33a6a6681784c
WSJI0 <lifedev@naver.com>
2020-07-16 12:39:06 +0900

    Update 10844.py

--- 10000-99999/10844.py
+++ 10000-99999/10844.py
@@ -8,17 +8,8 @@ input=sys.stdin.readline
 
 n=int(input())
 
-dp=[1,2,3,4,5,6,7,8,9]
+dp=[[0,0,0,0,0,0,0,0,0,0]*100]
 cnt=0
-while True:
-    a=[]
-    if n==1: break
-    for i in range(len(dp)):
-        if dp[i]>0:
-            a.append(dp[i]-1)
-        if dp[i]<9:
-            a.append(dp[i]+1)
-    dp=a  
-    cnt+=1
-    if cnt==n-1: break
-print(len(dp)%1000000000)
+
+for i in range(1, 10):
+    
