c19446c9a78624a13e545dcefb66e55d78886b18
WSJI0 <lifedev@naver.com>
2020-07-18 17:47:34 +0900

    Update 2573.py

--- 1000-9999/2573.py
+++ 1000-9999/2573.py
@@ -13,12 +13,14 @@ for _ in range(n):
     board.append(list(map(int, input().split())))
 
 q=deque()
+q2=deque()
 for y in range(n):
     for x in range(m):
         if board[y][x]!=0:
             q.append((x,y))
 
 year=0
+sep=False
 while True:
     year+=1
     visited={}
@@ -40,6 +42,23 @@ while True:
             if board[node[1]][node[0]]>0:
                 ice.append((node[0], node[1]))
     q=ice
-    print("--------------")
-    for i in board:
-        print(*i)
+    if len(q)==0: break
+    q2.append(q[0])
+    visited2={}
+    while q2:
+        node=q2.popleft()
+        if node not in visited2:
+            visited2[node]=True
+            for v in move:
+                mx=v[0]+node[0]
+                my=v[1]+node[1]
+                if board[my][mx]>0:
+                    q2.append((mx, my))
+    if len(q)!=len(visited2):
+        sep=True
+        break
+
+if sep:
+    print(year)
+else:
+    print(0)
