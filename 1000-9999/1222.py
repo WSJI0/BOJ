a164428da74dd55dff8173cc6a478fa318e42ee2
WSJI0 <lifedev@naver.com>
2020-07-19 22:33:18 +0900

    Update 1222.py

--- 1000-9999/1222.py
+++ 1000-9999/1222.py
@@ -6,7 +6,19 @@
 import sys
 input=sys.stdin.readline
 
-
 n=int(input())
 a=list(map(int, input().split()))
 
+num=[0]*2000002
+for i in a:
+    num[i]+=1
+
+ans=0
+for i in range(1, 2000001):
+    cnt=0
+    for j in range(i, 2000001, i):
+        cnt+=num[j]
+    if cnt>1:
+        ans=max(ans, cnt*i)
+
+print(ans)
