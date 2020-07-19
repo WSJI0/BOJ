f316c96670d3da0becbfc114f3f858fdbab95036
WSJI0 <lifedev@naver.com>
2020-07-19 22:33:33 +0900

    Update 9663.py

--- 1000-9999/9663.py
+++ 1000-9999/9663.py
@@ -3,49 +3,40 @@
 N-Queen
 '''
 
-n=int(input())
 board=[]
 
-for _ in range(n):
-    board.append([0]*n)
+n=int(input())
+for _ in range(n): board.append([0]*n)
 
-def check(a,b):
-    global board
-    if 1 in board[a]:
-        return False
-    for i in range(n):
-        if board[i][b]==1:
+def check(board, x, y):
+    for i in range(len(board)):
+        if board[y][i]==1:
             return False
-            break
-    for s1 in range(1,min(a,b)+1):
-        if board[a-s1][b-s1]==1:
+        elif board[i][x]==1:
             return False
-            break
+    for i in range(-min(x,y), min(n-x,n-y)):
+        if board[y+i][x+i]==1:
+            return False
+    for i in range(-min(x,n-y), min(n-x,y)):
+        if board[y-i-1][x+i]==1:
+            return False
 
-    for s2 in range(1,min(n-1-a,n-1-b)+1):
-        if board[a+s2][b+s2]==1:
-            return False
-            break
-
-    for s3 in range(1,min(n-1-a,b)+1):
-        if board[a+s3][b-s3]==1:
-            return False
-            break
-
-    for s4 in range(1,min(a,n-1-b)+1):
-        if board[a-s4][b+s4]==1:
-            return False
-            break
- 
     return True
+        
 
-for i in range(n):
-    for j in range(n):
-        print(i,j,check(i,j))
-        if (i!=0 and j!=0) and (i!=0 and j!=1) and (i!=0 and j!=2) and (i!=0 and j!=3):
-            if check(i,j):
-                board[i][j]=1
-                break
+cnt=0
+def backT(cur):
+    global cnt
 
-for k in board:
-    print(k)
+    if cur==n:
+        cnt+=1
+        return
+    
+    for y in range(n):
+        for x in range(n):
+            if not check(board, x, y): continue
+            board[y][x]=1
+            backT(cur+1)
+            board[y][x]=0
+backT(0)
+print(cnt)
