5330325cf37f5b0f1971c4a1ed96aea9fa25e75c
WSJI0 <lifedev@naver.com>
2020-07-22 20:48:30 +0900

    Update 10844.py

--- 10000-99999/10844.py
+++ 10000-99999/10844.py
@@ -1,6 +1,8 @@
 '''
 10844번
 쉬운 계단 수
+
+미완성
 '''
 
 import sys
@@ -8,8 +10,8 @@ input=sys.stdin.readline
 
 n=int(input())
 
-dp=[[0,0,0,0,0,0,0,0,0,0]*100]
+dp=[[0,0,0,0,0,0,0,0,0,0]*n]
 cnt=0
 
 for i in range(1, 10):
-    
+    dp[i]
