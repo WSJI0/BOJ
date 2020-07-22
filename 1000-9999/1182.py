85bdc1f1d1312120cf98b7b417740f68fa81ae78
WSJI0 <lifedev@naver.com>
2020-07-22 20:48:14 +0900

    Update 1182.py

--- 1000-9999/1182.py
+++ 1000-9999/1182.py
@@ -4,16 +4,22 @@
 '''
 
 import sys
+input=sys.stdin.readline
 
-n, s=map(int, sys.stdin.readline().split())
-a=list(map(int, sys.stdin.readline().split()))
+n, s=map(int, input().split())
+a=list(map(int, input().split()))
 
-S=[0]
-cnt=0
-for i in a:
-    for j in list(S):
-        S.append(i+j)
-        if S[-1]==s:
-            cnt+=1
+su=0
+ans=0
+def backT(d):
+    global n, s, a, su, ans
+    if d==n: return
+    if su+a[d]==s: ans+=1
 
-print(cnt)
+    backT(d+1)
+    su+=a[d]
+    backT(d+1)
+    su-=a[d]
+
+backT(0)
+print(ans)
