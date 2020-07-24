83f1e002897ba39fdeb6189b2191a6115012a928
WSJI0 <lifedev@naver.com>
2020-07-24 14:24:03 +0900

    Update 9663.py

--- 1000-9999/9663.py
+++ 1000-9999/9663.py
@@ -9,34 +9,33 @@ n=int(input())
 for _ in range(n): board.append([0]*n)
 
 def check(board, x, y):
-    for i in range(len(board)):
-        if board[y][i]==1:
+    global n
+    for i in range(y):
+        if board[i][x]:
             return False
-        elif board[i][x]==1:
+        if (y-i-1>=0 and x+i+1<n) and board[y-i-1][x+i+1]:
             return False
-    for i in range(-min(x,y), min(n-x,n-y)):
-        if board[y+i][x+i]==1:
-            return False
-    for i in range(-min(x,n-y), min(n-x,y)):
-        if board[y-i-1][x+i]==1:
+        if (y-i-1>=0 and x-i-1>=0) and board[y-i-1][x-i-1]:
+            if y-i-1<0 or x-i-1<0: continue
             return False
 
     return True
         
 
 cnt=0
+visited={}
 def backT(cur):
+    global board, visited
     global cnt
 
     if cur==n:
         cnt+=1
         return
     
-    for y in range(n):
-        for x in range(n):
-            if not check(board, x, y): continue
-            board[y][x]=1
-            backT(cur+1)
-            board[y][x]=0
+    for x in range(n):
+        if not check(board, x, cur): continue
+        board[cur][x]=1
+        backT(cur+1)
+        board[cur][x]=0
 backT(0)
 print(cnt)
