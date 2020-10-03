4aa9a23cde0907e4abc56f2dfdd8c62e1ef72f56
WSJI0 <lifedev@naver.com>
2020-10-04 01:33:35 +0900

    2020/10/04

---
+++ 10000-99999/15897.py
@@ -0,0 +1,8 @@
+'''
+15897번
+잘못 구현한 에라토스테네스의 체
+'''
+
+import sys
+input=sys.stdin.readline
+
--- 10000-99999/19623.py
+++ 10000-99999/19623.py
@@ -6,3 +6,13 @@
 import sys
 input=sys.stdin.readline
 
+n=int(input())
+conf=[]
+for _ in range(n):
+    conf.append(list(map(int, input().split())))
+
+conf.sort(key=lambda k:k[1])
+
+dp=[conf[0]]
+for i in range(1, n):
+    conf.
