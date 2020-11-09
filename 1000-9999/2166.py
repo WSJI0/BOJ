82805b4e2ff4de0ab7ebd13092581f2179ac262d
WSJI0 <lifedev@naver.com>
2020-11-09 22:21:10 +0900

    2020/11/09

--- 1000-9999/2166.py
+++ 1000-9999/2166.py
@@ -4,6 +4,17 @@
 '''
 
 import sys
+import math
 input=sys.stdin.readline
 
 n=int(input())
+coor=[]
+for _ in range(n):
+    coor.append(list(map(int, input().split())))
+
+ans=0
+for i in range(n-1):
+    ans+=(coor[i][0]+coor[i+1][0])*(coor[i][1]-coor[i+1][1])
+ans+=(coor[n-1][0]+coor[0][0])*(coor[n-1][1]-coor[0][1])
+
+print("%.1f" % math.fabs(ans/2))
---
+++ 1000-9999/2457.py
@@ -0,0 +1,15 @@
+'''
+2457번
+공주님의 정원
+'''
+
+import sys
+input=sys.stdin.readline
+
+n=int(input())
+md=[]
+for _ in range(n):
+    m1, d1, m2, d2=map(int, input().split())
+    md.append([m2, d2, m1, d1])
+md.sort()
+print(md)
