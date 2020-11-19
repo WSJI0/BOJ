98d30871c4e3e7aa674ba178ae746f1220c7ffc2
WSJI0 <lifedev@naver.com>
2020-11-20 01:03:47 +0900

    2020/11/20

--- 1000-9999/3665.py
+++ 1000-9999/3665.py
@@ -6,7 +6,7 @@
 '''
 
 import sys
-from collections import deque
+import heapq
 input=sys.stdin.readline
 
 t=int(input())
@@ -14,7 +14,7 @@ for _ in range(t):
     n=int(input())
     t=list(map(int, input().split()))
 
-    s=[0]*n
+    s=[0]*(n+1); indgree=[0]*(n+1)
     for i in range(n):
         s[t[i]]=i
 
@@ -25,7 +25,28 @@ for _ in range(t):
         if s[a]>s[b]:
             if a not in graph: graph[a]=[]
             graph[a].append(b)
+            indgree[b]+=1
         else:
             if b not in graph: graph[b]=[]
             graph[b].append(a)
-        
+            indgree[a]+=1
+    print(indgree)
+
+    q=[]
+    for i in range(1, n+1):
+        if indgree[i]==0:
+            heapq.heappush(q, [s[i], i])
+
+    ans=[]
+    while q:
+        node=heapq.heappop(q)[1]
+        if indgree[node]==0:
+            ans.append(node)
+        if node not in graph: continue
+        for g in graph[node]:
+            indgree[g]-=1
+            if indgree[g]==0:
+                heapq.heappush(q, [s[g], g])
+
+    print(*ans if len(ans)==n else "IMPOSSIBLE")
+    print("-----------")
