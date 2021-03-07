71454ee5108a81546f9285ef661f1216b6eb3b3e
WSJI0 <lifedev@naver.com>
2021-03-08 01:53:44 +0900

    2021/03/08

--- 1000-9999/1509.cpp
+++ 1000-9999/1509.cpp
@@ -5,15 +5,32 @@ using namespace std;
 
 string s;
 int dp[2501][2501];
+bool p[2501][2501];
+
+bool check(int l, int r){
+    if(l==r) return 1;
+    if(p[l][r]!=-1) return p[l][r];
+    if(r-1==l && s[l]==s[r]) return p[l][r]=1;
+    else p[l][r]=0;
+
+    if(r>l) return 0;
+
+    return p[l][r]=(check(l+1, r-1) && (s[l]==s[r]));
+}
 
 int solve(int l, int r){
     if(l==r) return 1;
-
     int& ret=dp[l][r];
+
     if(ret!=-1) return ret; 
+    ret=0;
+    if(p[l][r]==1) ret++;
+    
     for(int i=l; i<r; i++){
-        
+        int L=solve(l, i), R=solve(i+1, r);
+        if(L && R) ret++;
     }
+    return ret;
 }
 
 int main(void){
@@ -21,6 +38,7 @@ int main(void){
     
     cin>>s;
     memset(dp, -1, sizeof(dp));
+    memset(p, -1, sizeof(p));
 
     cout<<solve(0, s.size()-1)<<"\n";
 }
