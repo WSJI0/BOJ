45759c12b258309283269b93e169b375f1005c72
WSJI0 <lifedev@naver.com>
2021-04-23 00:04:42 +0900

    2021/04/23

--- 1000-9999/1028.cpp
+++ 1000-9999/1028.cpp
@@ -3,7 +3,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int r, c, dp[752][752][4], board[752][752], ans;
+
 int main(void){
-    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
     
+    scanf("%d %d", &r, &c);
+    for(int i=1; i<=r; i++){
+        for(int j=1; j<=c; j++) scanf("%1d", &board[i][j]);
+    }
+
+    for(int i=1; i<=r; i++){
+        for(int j=1; j<=c; j++){
+            int ii=r-i+1;
+            if(board[i][j]){
+                dp[i][j][0]=dp[i-1][j-1][0]+1;
+                dp[i][j][1]=dp[i-1][j+1][1]+1;
+            }
+            if(board[ii][j]){
+                dp[ii][j][2]=dp[ii+1][j+1][2]+1;
+                dp[ii][j][3]=dp[ii+1][j-1][3]+1;
+            }
+        }
+    }
+
+    for(int i=1; i<=r; i++){
+        for(int j=1; j<=c; j++){
+            int a=min(dp[i][j][2], dp[i][j][3]);
+            int b=min(dp[i][j][1], dp[i][j][2]);
+            for(int k=1; k<=a; k++){
+                if((k-1)*2+i>r) break;
+                if(dp[(k-1)*2+i][j][0]>=k && dp[(k-1)*2+i][j][1]>=k){
+                    ans=max(ans, k);
+                }
+            }
+            for(int k=1; k<=b; k++){
+                if((k-1)*2+j>c) break;
+                if(dp[i][(k-1)*2+j][0]>=k && dp[i][(k-1)*2+j][3]>=k){
+                    ans=max(ans, k);
+                }
+            }
+        }
+    }
+    
+    printf("%d", ans);
 }
--- Algorithm/test.cpp
+++ Algorithm/test.cpp
@@ -1,36 +1,44 @@
-// 14003 가장 긴 증가하는 부분 수열 5
-
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
+typedef int ll;
+ 
+ll arr[1508][1508];
+ll ld[808][808];
+ll rd[808][808];
+ll lu[808][808];
+ll ru[808][808];
+ 
+int main(){
+    ll i,j,k,l,m,n;
+    scanf("%d %d",&n,&m);
+    for(i=1;i<=n;i++)
+        for(j=1;j<=m;j++)
+            scanf("%1d",&arr[i][j]);
+ 
+    for(i=n;i>=1;i--)
+        for(j=1;j<=m;j++)
+            if(arr[i][j]==1){
+                ld[i][j]=ld[i+1][j-1]+1; //3
+                rd[i][j]=rd[i+1][j+1]+1; //2
+            }
+ 
+    for(i=1;i<=n;i++)
+        for(j=1;j<=m;j++)
+            if(arr[i][j]==1){
+                lu[i][j]=lu[i-1][j-1]+1; //0
+                ru[i][j]=ru[i-1][j+1]+1; //1
+            } 
 
-int n, a[1000001], b[1000001];
-vector<int> lis, ans;
-
-int main(void){
-    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    cin>>n;
-    for(int i=1; i<=n; i++) cin>>a[i];
-    lis.push_back(a[1]);
-
-    int cur=0;
-    for(int i=2; i<=n; i++){
-        if(lis[cur]<a[i]){
-            lis.push_back(a[i]);
-            b[i]=++cur;
-        } else{
-            int l=lower_bound(lis.begin(), lis.end(), a[i])-lis.begin();
-            b[i]=l;
-            lis[l]=a[i];
+    ll ans=0;
+    for(i=1;i<=n;i++)
+        for(j=1;j<=m;j++){
+            cout<<min(ru[i][j], rd[i][j])<<"\n";
+            for(k=1;k<=min(ld[i][j],rd[i][j]);k++)
+                if(arr[i+2*(k-1)][j]&&lu[i+2*(k-1)][j]>=k&&ru[i+2*(k-1)][j]>=k)
+                    ans=max(ans,k);
+            for(k=1;k<=min(ru[i][j],rd[i][j]);k++)
+                if(arr[i][j+2*(k-1)]&&lu[i][j+2*(k-1)]>=k&&ld[i][j+2*(k-1)]>=k)
+                    ans=max(ans,k);    
         }
-    }
-
-    cout<<cur+1<<"\n";
-    for(int i=n; i>=1 && cur>=0; i--){
-        if(b[i]==cur){
-            ans.push_back(a[i]);
-            cur--;
-        }
-    }
-    for(int i=ans.size()-1; i>=0; i--) cout<<ans[i]<<" ";
-    cout<<"\n";
+    printf("%d",ans);
 }
