70e8f8230b9fdd65a0f478e4566566265b988fb7
WSJI0 <lifedev@naver.com>
2021-05-26 19:50:28 +0900

    2021/05/26

--- 1000-9999/1014.cpp
+++ 1000-9999/1014.cpp
@@ -4,19 +4,24 @@
 using namespace std;
 
 int t, n, m, dp[11][11], ans=0;
-int mov[9][2]={
-    {}
+int mov[10][2]={
+    {0, -2}, {-1, -2}, {-2, -2}, {-2, -1}, {-2, 0}, {-2, 1}, {-2, 2}, {-1, 2}, {0, 2}, {-1, 0}
 };
 char board[11][11];
 
 int solve(int y, int x){
+    if(board[y][x]=='x') return 0;
     if(dp[y][x]!=0) return dp[y][x];
     
-    int& ret=dp[y][x];
-    if(y>2){
-        ret+=solve(y-2, x);
-        if(x>=2) ret+=solve();
+    int& ret=dp[y][x]; ret=1;
+    for(auto v:mov){
+        int my=v[0]+y;
+        int mx=v[1]+x;
+        if(my>=0 && my<n && mx>=0 && mx<m){
+            ret=max(ret, solve(my, mx)+1);
+        }
     }
+    return ret;
 }
 
 int main(void){
@@ -31,12 +36,17 @@ int main(void){
             for(int j=0; j<m; j++) cin>>board[i][j];
         }
 
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++) cout<<board[i][j];
+            cout<<"\n";
+        }
+
         for(int i=0; i<m; i++) 
             if(board[0][i]=='.') dp[0][i]=1;
         for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++) solve(i, j);
+            for(int j=0; j<m; j++) ans=max(ans, solve(i, j));
         }
 
-        cout<<ans<<"\n";
+        cout<<"ANS"<<ans<<"\n";
     }
 }
